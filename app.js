24f644645c8b1a2759e8f80989d1feaeed0182d0
dinesh <dnegi4196@gmail.com>
2026-03-19 18:58:14 +0530

    fix folder

--- app.js
+++ app.js
@@ -2,32 +2,37 @@ const http = require("http");
 const fs = require("fs");
 const os = require("os");
 
-const PORT = 3000;
-
 const server = http.createServer((req, res) => {
+
   if (req.url === "/api/info") {
     res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        hostname: os.hostname(),
-        uptime: process.uptime(),
-        platform: os.platform(),
-        time: new Date(),
-      })
-    );
-  } else {
+    res.end(JSON.stringify({
+      hostname: os.hostname(),
+      uptime: process.uptime(),
+      platform: os.platform(),
+      time: new Date()
+    }));
+  }
+
+  else if (req.url === "/") {
     fs.readFile("./public/index.html", (err, data) => {
-      res.end(data);
+      if (err) {
+        res.writeHead(500);
+        res.end("Error loading page");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
     });
   }
+
+  else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+
 });
 
-server.listen(PORT, () => {
+server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
-
-
-
-
-
-
