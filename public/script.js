ccdfcaf36e65a3b3b879b6d8278c39f3e584c352
dinesh <dnegi4196@gmail.com>
2026-03-20 10:50:04 +0530

    fix js loading issue

--- public/script.js
+++ public/script.js
@@ -1,7 +1,16 @@
-fetch("/api/info")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("info").innerHTML =
-      "Host: " + data.hostname + "<br>" +
-      "Time: " + data.time;
-  });
+window.onload = function () {
+  fetch("/api/info")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("info").innerHTML = `
+        <p><b>Hostname:</b> ${data.hostname}</p>
+        <p><b>Uptime:</b> ${Math.floor(data.uptime)} sec</p>
+        <p><b>Platform:</b> ${data.platform}</p>
+        <p><b>Time:</b> ${data.time}</p>
+      `;
+    })
+    .catch(error => {
+      document.getElementById("info").innerHTML = "Error loading data";
+      console.error(error);
+    });
+};
